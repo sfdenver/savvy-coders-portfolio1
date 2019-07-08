863f5f3e0b9079bf1a5126745d5a67d80def6b98
Steve Field <steve.field@live.com>
2019-07-08 16:43:06 -0500

    javascript practice in Projects html

---
+++ index.js
@@ -0,0 +1,28 @@
+const name = prompt("What is your name?");
+
+const checkName = function() {
+  if (name === "") {
+    name = prompt("What is your name, for real this time?");
+
+    checkName(); // repeats until a name exists
+    }
+};
+
+checkName(); // kicks off the name-checking the first time
+
+alert("Hello " + name);
+
+alert("Let's play a game");
+
+const gameName = prompt("What game should we play?");
+
+const whatGame = function() {
+  if (gameName === "") {
+    gameName = prompt("Its time to play a game! Pick a game now!");
+
+    whatGame(); // repeats until a name exists
+    }
+};
+whatGame(); //kicks off naming a game to play the first time
+
+aleart("Great - we will play" + gameName);

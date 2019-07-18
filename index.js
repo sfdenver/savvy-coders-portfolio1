ec45a64113d65a5915e82960ad2af4c7bc6f5da2
Steve Field <steve.field@live.com>
2019-07-17 20:57:04 -0500

    re-render added

--- index.js
+++ index.js
@@ -3,18 +3,53 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-
-const state = {
-    'title': 'this is the page Title!'
+const states = {
+    'home': {
+        'title': 'Title Page!'
+    },
+    'about': {
+        'title': 'About Page'
+    },
+    'contact': {
+        'title': 'Contact Page'
+    }
 };
 
-
-// functions are invoked to return the results for each
-document.querySelector('#root').innerHTML = `
-  ${Navigation()}
-  ${Header()}
-  ${Main()}
-  ${Footer()}
+function render(state){
+    // functions are invoked to return the results for each
+    document.querySelector('#root').innerHTML = `
+    ${Navigation(state)}
+    ${Header(state)}
+    ${Main(state)}
+    ${Footer(state)}
 `;
+    console.log('state came in as ', state);
+}
+render(states.home);
 
-console.log(Navigation, Header, Main, Footer);
+// Navigation listeners. Elements will not exist until page is rendered.
+const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
+
+// Use i as the index of navigation items. Index runs from 0 to 1 less than number of items
+let i = 0;
+
+while(i < navItems.length){
+    navItems[i].addEventListener('click',
+        function clickHandler(event){
+            event.preventDefault();
+            render(states[event.target.textContent.toLowerCase()]);
+            // console.log('hello');
+            console.log(event.target.textContent);
+
+            // console.log(i);
+        });
+    i += 1;
+}
+
+// Re-render our page based on what the user clicks in our navigation menu
+/*
+    1. Capture wat was clicked in navigation - what is the text of the element that was clicked
+    2. Access a piece of state based on said text
+    3. Show the user the new view (call Render function)
+
+    */

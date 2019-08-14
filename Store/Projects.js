5e11e66e690ee7b5b6b02ae3fae9c7e39738f518
Steve Field <steve.field@live.com>
2019-08-14 12:28:07 -0500

    added fizzbuzz project

--- Store/Projects.js
+++ Store/Projects.js
@@ -5,9 +5,58 @@ const project1 = {
     },
     'title': 'Project 1',
     'page': `
-    <h2>Project 1</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ullam cupiditate, et atque soluta eaque nobis, quasi rerum illum iusto veritatis maiores rem reiciendis quaerat minus velit, hic aspernatur. Reiciendis. </p>
-    `
+    <h2>Project 1 - FizzBuzz</h2>
+        <p>Part 1 - Performs FizzBuzz once
+        <br>1. Create function fizBuzz with one input
+        <br>2. If input number is divisible by 3 or 5, display FizzBuzz
+        <br>3. If input number is divisible by 3, display Fizz
+        <br>4. If input number is divisible by 5, display Buzz
+        <br>5. If input number is none of the above, and an integer, display Not divisible by 3 or 5
+        <br>6. all other cases, display error message Not an Integer
+        <br>
+        <br>Part 2 - place FizzBuzz in a While Loop up to 100
+        <br>
+        <br>View Console for results
+        </p>
+
+        <Script>
+
+        function fizzBuzz(num){
+            if(num % 3 === 0 && num % 5 === 0){
+                return 'FizzBuzz';
+            }
+            if(num % 3 === 0){
+                return 'Fizz';
+            }
+            if(num % 5 === 0){
+                return 'Buzz';
+            }
+
+            return 'Not divisible by 3 or 5';
+        }
+
+        // Part 1 validation
+        console.log('Part 1 FizzBuzz validation');
+        console.log(fizzBuzz(15));
+        console.log(fizzBuzz(3));
+        console.log(fizzBuzz(5));
+        console.log(fizzBuzz(4.1));
+        console.log(fizzBuzz('four'));
+
+        // Part 2 While loop
+        console.log('Part 2 FizzBuzz in a While loop');
+        let number2 = 0;
+        const incr = 2;
+
+        console.log(' Increment by ' + incr);
+
+        while(number2 <= 60){
+            console.log(number2 + '  ' + fizzBuzz(number2));
+            number2 += incr;
+        }
+        console.log('Part 2 is finished with While loop');
+        </Script>
+        `
 };
 
 const project2 = {
--- index.js
+++ index.js
@@ -4,194 +4,16 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 // import Navigo constructor function from the node 'navigo'
 import Navigo from 'navigo';
+import * as store from './store';
+console.log('Store came in as ', store);
 import Axios from 'axios';
 // import { Http2SecureServer } from 'http2';
 
 const router = new Navigo(location.origin);
 
-const store = {
-    'home': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-
-        'title': 'This is the home page!',
-        'page': `
-        <section>
-        <h2> Section 1 Header </h2>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.Provident,
-        magnam numquam corporis repudiandae porro tempore rerum dolores vitae dicta,
-        reiciendis distinctio,
-        impedit accusantium libero!Iusto mollitia minus ipsam quam laborum!
-        </p>
-        <a href = "#" class = "cta-btn" >Read More!</a>
-        </section>
-
-        <section>
-        <h2> Section 2 Header </h2>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.Provident,
-        magnam numquam corporis repudiandae porro tempore rerum dolores vitae dicta,
-        reiciendis distinctio, impedit accusantium libero!Iusto mollitia minus ipsam quam laborum!
-        </p>
-        <a href = "#" class = "cta-btn" > Read More! </a>
-        </section>
-
-        <section >
-        <h2> Section 3 Header </h2> <
-        p > Lorem ipsum dolor sit amet consectetur adipisicing elit.Provident,
-        magnam numquam corporis repudiandae porro tempore rerum dolores vitae dicta,
-        reiciendis distinctio,
-        impedit accusantium libero!Iusto mollitia minus ipsam quam laborum!
-        </p>
-        <a href = "#" class = "cta-btn" >Read More! </a>
-
-        </section>
-        `
-    },
-
-    'about': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-        'title': 'About Page',
-        'page': `
-        <h2>About Me</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ullam cupiditate, et atque soluta eaque nobis, quasi rerum illum iusto veritatis maiores rem reiciendis quaerat minus velit, hic aspernatur. Reiciendis. </p>
-        `
-    },
-    'contact': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-        'title': 'Contact Page',
-        'page': `
-        <!--
-        The action attribute defines where on the server the form data should be sent
-        The method attribute specifies the HTTP method (GET or POST)
-        -->
-
-        <form action="form-responses/new" method="POST">
-            <div class="input-group">
-
-            <!-- Developer's Note: 'placeholder' without using < label > is NOT RECOMMENDED for accessibility purposes. -->
-            <input type="text" name="firstname" placeholder="First Name" autofocus required />
-            <input type="text" name="lastname" placeholder="Last Name" />
-            <input type="email" name="userEmail" placeholder="your.email@example.com" />
-            </div>
-
-            <p>What's this message about?</p>
-
-            <div class="input-group">
-                <span>
-                    <input type="radio" name="subject" value="professional" id="pro" checked />
-                    <label for="pro">I'd like to hire you!</label>
-                </span>
-
-                <span>
-                    <input type="radio" name="subject" value="personal" id="personal" />
-                    <label for="personal">Personal message</label>
-                </span>
-
-                <span>
-                    <input type="radio" name="subject" value="other" />
-                    <label>Don't know/something else</label>
-                </span>
-            </div>
-
-            <div class="input-group">
-                <input type="checkbox" name="optin" value="trusting" checked />Subscribe me to
-                    your newsletter!
-                <input type="checkbox" name="optout" value="skeptical" disabled />Not at this time
-            </div>
-
-            <div class="input-group input-group--flex">
-
-                <label for="marketing">How did you hear about me?</label>
-                    <select name="marketing">
-                    <optgroup label="Online">
-                        <option value="social">Social Media (FB, Twitter, LinkedIn)</option>
-                        <option value="github">Online Portfolio (GitHub)</option>
-                        <option value="search">Search Engine</option>
-                        <option value="email">Email</option>
-                    </optgroup>
-                    <optgroup label="In-Person">
-                        <option value="networking">We met at a networking event</option>
-                        <option value="referral">Personal referral</option>
-                        <option value="random">We met somewhere else</option>
-                    </optgroup>
-                        <option value="other">Other</option>
-                    </select>
-            </div>
-
-            <div class="input-group--flex">
-                <textarea name="user_message" placeholder="Please provide comments and feedback!" rows="8" cols="40"></textarea>
-                <div>
-                    <input type="submit" />
-                    <input type="reset" value="Reset">
-                </div>
-            </div>
-        </form>
-
-        `
-    },
-    'blog': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-        'title': 'My Blog Page',
-        //
-        'page': `
-        <h2>Blog Posts</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ullam cupiditate, et atque soluta eaque nobis, quasi rerum illum iusto veritatis maiores rem reiciendis quaerat minus velit, hic aspernatur. Reiciendis. </p>
-    `
-    },
-
-    // Sections for Project Placeholders
-    'project 1': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-        'title': 'Project 1',
-        'page': `
-        <h2>Project 1</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ullam cupiditate, et atque soluta eaque nobis, quasi rerum illum iusto veritatis maiores rem reiciendis quaerat minus velit, hic aspernatur. Reiciendis. </p>
-        `
-    },
-
-    'project 2': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-        'title': 'Project 2',
-        'page': `
-        <h2>Project 2</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ullam cupiditate, et atque soluta eaque nobis, quasi rerum illum iusto veritatis maiores rem reiciendis quaerat minus velit, hic aspernatur. Reiciendis. </p>
-        `
-    },
-
-    'project 3': {
-        'links': {
-            'primary': [ 'Home', 'About', 'Contact', 'Blog' ],
-            'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
-        },
-        'title': 'Project 3',
-        'page': `
-        <h2>Project 3</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ullam cupiditate, et atque soluta eaque nobis, quasi rerum illum iusto veritatis maiores rem reiciendis quaerat minus velit, hic aspernatur. Reiciendis. </p>
-        `
-    },
-
-
-};
-
 function render(state){
     // functions are invoked to return the results for each
+    console.log('render using state = ', state);
     document.querySelector('#root').innerHTML = `
       ${Navigation(state)}
       ${Header(state)}
@@ -225,6 +47,7 @@ navItems.forEach(function eventListenerAdder(navItem){
 */
 
 // request to render a page passes in a selected state.
+console.log(store.home);
 render(store.home);
 
 /**
@@ -242,7 +65,7 @@ router
     })
     .resolve();
 
-
+/*
 axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
     const blogPosts = response.data;
     const blogHTML = blogPosts
@@ -258,6 +81,7 @@ axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
 
     store.blog.page = blogHTML;
 });
+*?
 
 /**
 fetch('https://jsonplaceholder.typicode.com/posts')
